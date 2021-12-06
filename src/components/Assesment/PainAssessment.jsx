59b2b28fa36755f5e967d35f8650564be7b13790
unknown <aswin@WWW.MYPHYSIO.AI>
2021-12-07 01:52:16 +0530

    pain assessment data added to api and episode changes

--- src/components/Assesment/PainAssessment.jsx
+++ src/components/Assesment/PainAssessment.jsx
@@ -8,6 +8,7 @@ import Body from "./Body";
 import DummyBody from "./DummyBody";
 import { FrownOutlined, MehOutlined, SmileOutlined } from "@ant-design/icons";
 import { Label } from "devextreme-react/chart";
+import { useHistory } from "react-router";
 const marks = {
   0: <i class="far fa-smile" style={{ fontSize: 25 }}></i>,
   2: <i class="far fa-smile" style={{ fontSize: 25, color: "lime" }}></i>,
@@ -19,6 +20,7 @@ const marks = {
   10: <i class="far fa-tired" style={{ fontSize: 25, color: "red" }}></i>,
 };
 const PainAssessment = () => {
+  const history = useHistory();
   const [form] = Form.useForm();
   // const[value, setValue]=useState();
   const state = useSelector((state) => state);
@@ -26,14 +28,141 @@ const PainAssessment = () => {
 
   useEffect(() => {}, [assesmentstate]);
   const dispatch = useDispatch();
-  const handleChange1 = (key, value, id = 0) => {
-    dispatch({
-      type: STATECHANGE,
-      payload: {
-        key,
-        value,
-      },
-    });
+  const [test, setTest] = useState({
+    Aching: 0,
+    Burning: 0,
+    Stabbing: 0,
+    Needles: 0,
+    Numbness: 0,
+    Shooting: 0,
+    Stiffness: 0,
+  });
+  const [test1, setTest1] = useState({
+    sleep: 0,
+    Rest: 0,
+    Movement: 0,
+    Jerk: 0,
+  });
+  const handleChange1 = async (key, value, id = 0) => {
+    if (key === "nature_of_pain") {
+      if (value == "Aching") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Aching: 1 },
+          },
+        });
+      } else if (value === "Burning") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Burning: 1 },
+          },
+        });
+      } else if (value === "Stabbing") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Stabbing: 1 },
+          },
+        });
+      } else if (value === "Needles") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Needles: 1 },
+          },
+        });
+      } else if (value === "Numbness") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Numbness: 1 },
+          },
+        });
+      } else if (value === "Shooting") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Shooting: 1 },
+          },
+        });
+      } else if (value === "Stiffness") {
+        dispatch({
+          type: STATECHANGE,
+          payload: {
+            key,
+            value: { ...test, Stiffness: 1 },
+          },
+        });
+      }
+    } else if (key === "pain_aggravating") {
+      let obj = {
+        Sleep: 0,
+        Rest: 0,
+        Movement: 0,
+        Jerk: 0,
+      };
+      if (value.includes("Sleep")) {
+        obj["Sleep"] = 1;
+      }
+      if (value.includes("Rest")) {
+        obj["Rest"] = 1;
+      }
+      if (value.includes("Movement")) {
+        obj["Movement"] = 1;
+      }
+      if (value.includes("Jerk")) {
+        obj["Jerk"] = 1;
+      }
+      dispatch({
+        type: STATECHANGE,
+        payload: {
+          key,
+          value: obj,
+        },
+      });
+    } else if (key === "pain_relieving") {
+      let obj = {
+        Rest: 0,
+        Hot: 0,
+        Medication: 0,
+        Physiotherapy: 0,
+      };
+      if (value.includes("Rest")) {
+        obj["Rest"] = 1;
+      }
+      if (value.includes("Hot")) {
+        obj["Hot"] = 1;
+      }
+      if (value.includes("Medication")) {
+        obj["Medication"] = 1;
+      }
+      if (value.includes("Physiotherapy")) {
+        obj["Physiotherapy"] = 1;
+      }
+      dispatch({
+        type: STATECHANGE,
+        payload: {
+          key,
+          value: obj,
+        },
+      });
+    } else {
+      dispatch({
+        type: STATECHANGE,
+        payload: {
+          key,
+          value,
+        },
+      });
+    }
     dispatch({ type: "NOERROR" });
   };
   const onChange = (e) => {
@@ -53,7 +182,7 @@ const PainAssessment = () => {
     // history.goBack();
     alert("hello");
   };
-  const desc = ["no pain", "mild", "moderate", "severe"];
+  const desc = [1, 2, 3, 4];
   const formatter = (value) => {
     return `${desc[value]}`;
   };
@@ -63,9 +192,11 @@ const PainAssessment = () => {
     2: <FrownOutlined style={{ fontSize: 25, color: "orange" }} />,
     3: <i class="far fa-tired" style={{ fontSize: 25, color: "red" }}></i>,
   };
-  const plainOptions = ["Rest", "Movement", "Jerk","Sleep"];
+  const plainOptions = ["Rest", "Movement", "Jerk", "Sleep"];
   const plainOptions1 = ["Rest", "Hot", "Medication", "Physiotherapy"];
-
+  const goBack =() =>{
+    history.push('/assessment/1')
+  }
   return (
     <div className="px-2 py-2">
       <Row>
@@ -94,14 +225,17 @@ const PainAssessment = () => {
               <Radio.Group
                 name="Nature Of Pain"
                 style={{ paddingLeft: "20px" }}
+                onChange={(e) =>
+                  handleChange1("nature_of_pain", e.target.value)
+                }
               >
-                <Radio value={1}>aching</Radio>
-                <Radio value={2}>Burning</Radio>
-                <Radio value={3}>Stabbing</Radio>
-                <Radio value={4}>& Needles</Radio>
-                <Radio value={5}>NumbNess</Radio>
-                <Radio value={6}>Shooting</Radio>
-                <Radio value={7}>Stiffness</Radio>
+                <Radio value={"Aching"}>Aching</Radio>
+                <Radio value={"Burning"}>Burning</Radio>
+                <Radio value={"Stabbing"}>Stabbing</Radio>
+                <Radio value={"Needles"}>Needles</Radio>
+                <Radio value={"Numbness"}>Numbness</Radio>
+                <Radio value={"Shooting"}>Shooting</Radio>
+                <Radio value={"Stiffness"}>Stiffness</Radio>
               </Radio.Group>
             </Col>
             <Col
@@ -125,9 +259,9 @@ const PainAssessment = () => {
                   max={3}
                   step={1}
                   tipFormatter={formatter}
-                  // onChange={(value) => handleChange1("Swelling", desc[value])}
-                  // defaultValue={desc.indexOf(state.FirstAssesment.Swelling)}
-                  // value={desc.indexOf(state.FirstAssesment.Swelling)}
+                  onChange={(value) => handleChange1("pain_scale", desc[value])}
+                  defaultValue={desc.indexOf(state.FirstAssesment.pain_scale)}
+                  value={desc.indexOf(state.FirstAssesment.pain_scale)}
                   style={{ width: 200 }}
                 />
               </div>
@@ -148,6 +282,7 @@ const PainAssessment = () => {
               <Checkbox.Group
                 style={{ paddingLeft: "20px" }}
                 options={plainOptions}
+                onChange={(e) => handleChange1("pain_aggravating", e)}
                 name="Pain Aggravating"
               />
             </Col>
@@ -167,6 +302,7 @@ const PainAssessment = () => {
               <Checkbox.Group
                 style={{ paddingLeft: "20px" }}
                 name="Pain Relieving"
+                onChange={(e) => handleChange1("pain_relieving", e)}
                 options={plainOptions1}
               />
             </Col>
@@ -187,15 +323,18 @@ const PainAssessment = () => {
                   style={{ paddingTop: "20px" }}
                   className="mt-2 AreasMain"
                 >
-                 Superficial : 
-                    <Radio.Group
-                      name="Superficial"
-                      style={{ paddingLeft: "20px" }}
-                    >
-                      <Radio value={1}>Intact</Radio>
-                      <Radio value={2}>Impaired</Radio>
-                      <Radio value={3}>Absent</Radio>
-                    </Radio.Group>
+                  Superficial :
+                  <Radio.Group
+                    name="Superficial"
+                    style={{ paddingLeft: "20px" }}
+                    onChange={(e) =>
+                      handleChange1("superficial", e.target.value)
+                    }
+                  >
+                    <Radio value={"Intact"}>Intact</Radio>
+                    <Radio value={"Impaired"}>Impaired</Radio>
+                    <Radio value={"Absent"}>Absent</Radio>
+                  </Radio.Group>
                 </Col>
                 <Col
                   md={24}
@@ -205,15 +344,16 @@ const PainAssessment = () => {
                   style={{ paddingTop: "20px" }}
                   className="mt-2 AreasMain"
                 >
-                Deep :
+                  Deep :
                   <Radio.Group
-                      name="Deep"
-                      style={{ paddingLeft: "53px" }}
-                    >
-                      <Radio value={1}>Intact</Radio>
-                      <Radio value={2}>Impaired</Radio>
-                      <Radio value={3}>Absent</Radio>
-                    </Radio.Group>
+                    onChange={(e) => handleChange1("deep", e.target.value)}
+                    name="Deep"
+                    style={{ paddingLeft: "53px" }}
+                  >
+                    <Radio value={"Intact"}>Intact</Radio>
+                    <Radio value={"Impaired"}>Impaired</Radio>
+                    <Radio value={"Absent"}>Absent</Radio>
+                  </Radio.Group>
                 </Col>
                 <Col
                   md={24}
@@ -225,18 +365,19 @@ const PainAssessment = () => {
                 >
                   Cortial :
                   <Radio.Group
-                      name="Cortial"
-                      style={{ paddingLeft: "45px" }}
-                    >
-                      <Radio value={1}>Intact</Radio>
-                      <Radio value={2}>Impaired</Radio>
-                      <Radio value={3}>Absent</Radio>
-                    </Radio.Group>
+                    onChange={(e) => handleChange1("cortial", e.target.value)}
+                    name="Cortial"
+                    style={{ paddingLeft: "45px" }}
+                  >
+                    <Radio value={"Intact"}>Intact</Radio>
+                    <Radio value={"Impaired"}>Impaired</Radio>
+                    <Radio value={"Absent"}>Absent</Radio>
+                  </Radio.Group>
                 </Col>
               </Card>
             </Col>
             <div className="text-center mb-3">
-              <button>Submit</button>
+              <button onClick={goBack}>Submit</button>
             </div>
           </Form>
         </Col>
