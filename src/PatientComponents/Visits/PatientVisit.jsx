9e2214bd63f47e013dbaf43c9572cee1d9e72413
Sushant Shetty <Sushant.Shetty@digitaldarwin.ai>
2022-09-02 13:48:44 +0000

    Patient Side

--- src/PatientComponents/Visits/PatientVisit.jsx
+++ src/PatientComponents/Visits/PatientVisit.jsx
@@ -1,6 +1,6 @@
 import moment from "moment";
 import React, { useState, useEffect } from "react";
-import { Radio } from "antd";
+import { Radio, Button, Tooltip } from "antd";
 import { fetchVisits } from "../../API/episode-visit-details/episode-visit-api";
 import { getEpisode } from "../../API/Episode/EpisodeApi";
 import { GetPatientCurrentEpisode } from "../../PatientAPI/PatientDashboardApi";
@@ -8,23 +8,26 @@ import "./PatientVisits.css";
 
 const PatientVisit = () => {
   const [allvisits, Setvisits] = useState([]);
-  const [visitShow, SetvisitShow] = useState('a');
+  const [visitShow, SetvisitShow] = useState("a");
+  const [doctor, SetDoctor] = useState("");
   useEffect(async () => {
     let userId = JSON.parse(localStorage.getItem("userId"));
     const patientVisits = await fetchVisits(userId);
-    let res
-    if(patientVisits.length > 0 ) res = await GetPatientCurrentEpisode();
-    console.log(res[1][0])
-    let name =res[1][0]["treating_doctor_detail"][0]["middle_name"] !== ""
-    ? res[1][0]["treating_doctor_detail"][0]["first_name"] +
-      " " +
-      res[1][0]["treating_doctor_detail"][0]["middle_name"] +
-      " " +
-      res[1][0]["treating_doctor_detail"][0]["last_name"]
-    : res[1][0]["treating_doctor_detail"][0]["first_name"] +
-      " " +
-      res[1][0]["treating_doctor_detail"][0]["last_name"];
-      console.log(name)
+    let res;
+    if (patientVisits.length > 0) res = await GetPatientCurrentEpisode();
+    console.log(res[1][0]);
+    let name =
+      res[1][0]["treating_doctor_detail"][0]["middle_name"] !== ""
+        ? res[1][0]["treating_doctor_detail"][0]["first_name"] +
+          " " +
+          res[1][0]["treating_doctor_detail"][0]["middle_name"] +
+          " " +
+          res[1][0]["treating_doctor_detail"][0]["last_name"]
+        : res[1][0]["treating_doctor_detail"][0]["first_name"] +
+          " " +
+          res[1][0]["treating_doctor_detail"][0]["last_name"];
+    console.log(name);
+    SetDoctor(name);
     const sortVisits = patientVisits.filter((data) => {
       //    console.log(data)
       const datetypof = typeof data.appointment_detail.startDate;
@@ -39,7 +42,7 @@ const PatientVisit = () => {
         return data;
       }
     });
-    const othersortVisits = patientVisits.filter((data) => {
+    let othersortVisits = patientVisits.filter((data) => {
       //    console.log(data)
       const datetypof = typeof data.appointment_detail.startDate;
       const date =
@@ -52,65 +55,176 @@ const PatientVisit = () => {
         return data;
       }
     });
-     Setvisits(visitShow === 'a' ?  sortVisits : othersortVisits);
+    othersortVisits = othersortVisits.sort(function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      return new Date(a.appointment_detail.startDate) - new Date(b.appointment_detail.startDate);
+    });
+    Setvisits(visitShow === "a" ? sortVisits : othersortVisits);
   }, [visitShow]);
   return (
     <>
-      {/* <p
-        className="w-100 text-start"
-        style={{
-          textAlign: "center",
-          fontSize: "30px",
-          position: "relative",
-          left: "10px",
-        }}
+      <Radio.Group
+        defaultValue="a"
+        onChange={(e) => SetvisitShow(e.target.value)}
+        size="large"
+        buttonStyle="solid"
       >
-        <b>Today</b>
-      </p> */}
-      <Radio.Group defaultValue="a" onChange={(e)=>SetvisitShow(e.target.value)} size="large" buttonStyle="solid">
-        <Radio.Button value="a" style={{margin:'10px',borderRadius:'20px'}}>Today</Radio.Button>
-        <Radio.Button value="b" style={{margin:'10px',borderRadius:'20px',marginLeft:'-2px'}}>Upcoming</Radio.Button>
+        <Radio.Button
+          value="a"
+          style={{ margin: "10px", borderRadius: "20px" }}
+        >
+          Today
+        </Radio.Button>
+        <Radio.Button
+          value="b"
+          style={{ margin: "10px", borderRadius: "20px", marginLeft: "-2px" }}
+        >
+          Upcoming
+        </Radio.Button>
       </Radio.Group>
       {allvisits.map((i) => (
-        <div className="p-2" id="visitCard">
-          <p
-            className="w-100 text-start"
-            style={{
-              textAlign: "left",
-              fontSize: "24px",
-              position: "relative",
-              left: "10px",
-            }}
-          >
-            <b>Visit Details</b>
-          </p>
-          <div className="upper-visit-row">
-            <span className="visit-col" span={10}>
-              {" "}
-              <b>Visit Timing:</b> {i.appointment_detail.start_time}
-            </span>
-            <span className="visit-col" style={{ float: "right" }} span={10}>
-              {" "}
-              <b>Visit Duration:</b> {i.appointment_detail.duration}{" "}
-            </span>
-          </div>
-          <div className="upper-visit-row">
-            <span className="visit-col" span={12}>
-              <b>Visit Type:</b> {i.visit_type}
-            </span>
-            <span className="visit-col" style={{ float: "right" }} span={12}>
-              <b>Visit Location:</b> {i.location}
-            </span>
-          </div>
-          {i.video_link !== "" && (
-            <div className="video-conference-detail">
-              <span className="video-col">
-                <b> Video Conference Detail:</b>{" "}
-                <a href={i.video_link}>{i.video_link}</a>
-              </span>
+        <>
+          {visitShow === "a" ? (
+            <div className="p-2" id="visitCard">
+              <img
+                src="https://cdn.pixabay.com/photo/2017/03/14/03/20/woman-2141808__340.jpg"
+                className="visitImage"
+                alt=""
+              />
+              <div className="visitDetails">
+                <p className="w-100 text-start doctorName">
+                  <b>Dr. {doctor}</b>
+                </p>
+                <div>
+                  <span className="visit-col" span={10}>
+                    {" "}
+                    <b>Time:</b> {i.appointment_detail.start_time}
+                  </span>
+                  <span className="visit-col colRight" span={10}>
+                    {" "}
+                    <b>Duration:</b> {i.appointment_detail.duration}{" "}
+                  </span>
+                </div>
+                <div>
+                  <span className="visit-col" span={12}>
+                    <b>Type:</b> {i.visit_type}
+                  </span>
+                  <span className="visit-col colRight" span={12}>
+                    <b>Location:</b> {i.location}
+                  </span>
+                </div>
+                {i.video_link !== "" && (
+                  <div
+                    className="video-conference-detail"
+                    style={{ float: "right" }}
+                  >
+                    <span className="video-col">
+                      {visitShow === "a" ? (
+                        <Button
+                          className="channelButton"
+                          type="primary"
+                          href={i.video_link}
+                          shape={"round"}
+                          target="_blank"
+                        >
+                          Join Channel
+                        </Button>
+                      ) : (
+                        <Tooltip title="You can only join on the same date">
+                          <Button
+                            className="channelButton"
+                            type="primary"
+                            href={i.video_link}
+                            shape={"round"}
+                            target="_blank"
+                            disabled
+                          >
+                            Join Channel
+                          </Button>
+                        </Tooltip>
+                      )}
+                      {/* <b> Video Conference Detail:</b>{" "}
+                  <a href={i.video_link}>{i.video_link}</a> */}
+                    </span>
+                  </div>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="p-2" id="visitCard">
+              <img
+                src="https://cdn.pixabay.com/photo/2017/03/14/03/20/woman-2141808__340.jpg"
+                className="visitImage"
+                alt=""
+              />
+              <div className="visitDetails">
+                <p className="w-100 text-start doctorName">
+                  <b>Dr. {doctor}</b>
+                </p>
+                <div>
+                  <span className="visit-col" span={10}>
+                    {" "}
+                    <b>Date:</b> {moment(i.appointment_detail.startDate).format('DD MMM YYYY')}
+                  </span>
+                  <span className="visit-col colRight" span={10}>
+                    {" "}
+                    <b>Time:</b> {i.appointment_detail.start_time}
+                  </span>
+                </div>
+                <div>
+                <span className="visit-col " span={10}>
+                    {" "}
+                    <b>Duration:</b> {i.appointment_detail.duration}{" "}
+                  </span>
+                  <span className="visit-col colRight" span={12}>
+                    <b>Type:</b> {i.visit_type}
+                  </span>
+                </div>
+                <div>
+                  <span className="visit-col" span={12}>
+                    <b>Location:</b> {i.location}
+                  </span>
+                </div>
+                {i.video_link !== "" && (
+                  <div
+                    className="video-conference-detail"
+                    style={{ float: "right" }}
+                  >
+                    <span className="video-col">
+                      {visitShow === "a" ? (
+                        <Button
+                          className="channelButton"
+                          type="primary"
+                          href={i.video_link}
+                          shape={"round"}
+                          target="_blank"
+                        >
+                          Join Channel
+                        </Button>
+                      ) : (
+                        <Tooltip title="You can only join on the same date">
+                          <Button
+                            className="disablechannelButton"
+                            type="primary"
+                            href={i.video_link}
+                            shape={"round"}
+                            target="_blank"
+                            disabled
+                          >
+                            Join Channel
+                          </Button>
+                        </Tooltip>
+                      )}
+                      {/* <b> Video Conference Detail:</b>{" "}
+                  <a href={i.video_link}>{i.video_link}</a> */}
+                    </span>
+                  </div>
+                )}
+              </div>
             </div>
           )}
-        </div>
+        </>
       ))}
     </>
   );
